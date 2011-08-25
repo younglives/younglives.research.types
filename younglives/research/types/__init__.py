cda75f33711dc6b7cad749aa00b02b7d2d556901
Michael Davis <m.r.davis@me.com>
2011-08-25 19:57:59 +0000

    Actually add the types

--- younglives/research/types/__init__.py
+++ younglives/research/types/__init__.py
@@ -1,3 +1,25 @@
+from zope.i18nmessageid import MessageFactory
+
+from Products.Archetypes import atapi
+from Products.CMFCore import utils
+
+from younglives.research.types import config
+
+researchMessageFactory = MessageFactory('younglives.research.types')
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+
+    from content.research import Research
+    from content.research_database import ResearchDatabase
+
+    content_types, constructors, ftis = atapi.process_types(
+        atapi.listTypes(config.PROJECTNAME),
+        config.PROJECTNAME)
+
+    for atype, constructor in zip(content_types, constructors):
+        utils.ContentInit('%s: %s' % (config.PROJECTNAME, atype.portal_type),
+            content_types=(atype, ),
+            permission=config.ADD_PERMISSIONS[atype.portal_type],
+            extra_constructors=(constructor,),
+            ).initialize(context)
