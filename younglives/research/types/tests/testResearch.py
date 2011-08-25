06b747001ef0e3582247275099b5f4ca3856051d
Michael Davis <m.r.davis@me.com>
2011-08-25 19:59:40 +0000

    Add basic tests for the types

--- younglives/research/types/tests/testResearch.py
+++ younglives/research/types/tests/testResearch.py
@@ -1,5 +1,8 @@
 import unittest2 as unittest
 
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+
 from base import YOUNGLIVES_RESEARCH_TYPES_INTEGRATION_TESTING
 
 class TestContentType(unittest.TestCase):
@@ -8,3 +11,10 @@ class TestContentType(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer['portal']
+
+    def testAddType(self):
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal.invokeFactory('ResearchDatabase', 'rd1')
+        rd1 = getattr(self.portal, 'rd1')
+        rd1.invokeFactory('Research', 'r1')
+        assert 'r1' in rd1.objectIds()
--- younglives/research/types/tests/testResearchDatabase.py
+++ younglives/research/types/tests/testResearchDatabase.py
@@ -1,5 +1,8 @@
 import unittest2 as unittest
 
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+
 from base import YOUNGLIVES_RESEARCH_TYPES_INTEGRATION_TESTING
 
 class TestContentType(unittest.TestCase):
@@ -8,3 +11,9 @@ class TestContentType(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer['portal']
+
+    def testAddType(self):
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal.invokeFactory('ResearchDatabase', 'rd1')
+        rd1 = getattr(self.portal, 'rd1')
+        assert 'rd1' in self.portal.objectIds()
