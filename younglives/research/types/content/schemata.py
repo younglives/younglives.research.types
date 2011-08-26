765c8aa59a325954556f02affec4bf6d366b4a8b
Michael Davis <m.r.davis@me.com>
2011-08-26 22:00:02 +0000

    Add the first field and import the vocabs

--- younglives/research/types/content/schemata.py
+++ younglives/research/types/content/schemata.py
@@ -5,11 +5,23 @@ from Products.Archetypes.atapi import Schema
 from Products.ATContentTypes.content.schemata import ATContentTypeSchema
 from Products.ATContentTypes.content.schemata import finalizeATCTSchema
 
+from younglives.research.types.config import RESEARCH_THEME, RESEARCH_METHODOLOGY, RESEARCH_COUNTRY, RESEARCH_OUTPUT
+
 ResearchDatabaseSchema = ATFolderSchema.copy() + Schema((
 
 ))
 
 ResearchSchema = ATContentTypeSchema.copy() + Schema((
+
+    StringField('researchTheme',
+        required = False,
+        searchable = False,
+        vocabulary = RESEARCH_THEME,
+        multiValued = True,
+        widget = SelectionWidget(
+            label='Reasearch Theme',
+        )
+    ),
 
 ))
 
