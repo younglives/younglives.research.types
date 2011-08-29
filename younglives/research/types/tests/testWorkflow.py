7c095f56dd5659f2221a7d9ce3f33797cf5ba79b
Michael Davis <m.r.davis@me.com>
2011-08-29 11:14:19 +0000

    Add states and transitions to the tests

--- younglives/research/types/tests/testWorkflow.py
+++ younglives/research/types/tests/testWorkflow.py
@@ -20,9 +20,33 @@ class TestInstallation(unittest.TestCase):
     def testStatesExist(self):
         research_workflow = self.research_workflow
         all_states = research_workflow['states'].objectIds()
+        assert len(all_states) == 11
         assert 'completed' in all_states
+        assert 'draft' in all_states
+        assert 'external-review' in all_states
+        assert 'in-production' in all_states
+        assert 'internal-review' in all_states
+        assert 'peer-review' in all_states
+        assert 'planned' in all_states
+        assert 'propsosed' in all_states
+        assert 'published' in all_states
+        assert 'rejected' in all_states
+        assert 'unpublished' in all_states
 
     def testTransitionsExist(self):
         research_workflow = self.research_workflow
         all_transitions = research_workflow['transitions'].objectIds()
+        assert len(all_transitions) == 13
+        assert 'accept' in all_transitions
         assert 'complete' in all_transitions
+        assert 'deline' in all_transitions
+        assert 'external-review' in all_transitions
+        assert 'internal-review' in all_transitions
+        assert 'peer-review' in all_transitions
+        assert 'produce' in all_transitions
+        assert 'propose' in all_transitions
+        assert 'publish' in all_transitions
+        assert 'redraft' in all_transitions
+        assert 'refused' in all_transitions
+        assert 'reject' in all_transitions
+        assert 'retrieve' in all_transitions
