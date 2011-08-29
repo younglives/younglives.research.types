d048fc308a567dde318d3e47b013809802381a01
Michael Davis <m.r.davis@me.com>
2011-08-29 18:28:29 +0000

    Add a simple test for the schema

--- younglives/research/types/tests/testResearch.py
+++ younglives/research/types/tests/testResearch.py
@@ -18,3 +18,22 @@ class TestContentType(unittest.TestCase):
         rd1 = getattr(self.portal, 'rd1')
         rd1.invokeFactory('Research', 'r1')
         assert 'r1' in rd1.objectIds()
+
+class TestContentSchema(unittest.TestCase):
+    """Test content type schema"""
+    layer = YOUNGLIVES_RESEARCH_TYPES_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal.invokeFactory('ResearchDatabase', 'rd1')
+        self.rd1 = getattr(self.portal, 'rd1')
+        self.rd1.invokeFactory('Research', 'r1')
+        self.r1 = getattr(self.rd1, 'r1')
+
+    def testSchema(self):
+        r1 = self.r1
+        schema = r1.schema
+        field_ids = schema.keys()
+        assert 'referenceNumber' in field_ids
+        reference_field = schema['referenceNumber']
