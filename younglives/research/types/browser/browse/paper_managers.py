45f663b8f4b049b99f953558445fea34c5a2b7df
Michael Davis <m.r.davis@me.com>
2011-09-06 20:56:16 +0000

    Add the inital paper manager form

---
+++ younglives/research/types/browser/browse/paper_managers.py
@@ -0,0 +1,29 @@
+from Products.Five.browser import BrowserView
+from Products.CMFCore.utils import getToolByName
+
+from younglives.research.types.config import RESEARCH_THEME
+
+class BrowsePaperManagersView(BrowserView):
+    """View to browse the research database by paper managers"""
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.request.set('disable_border', True)
+        self.selectedManager = self.request.form.get('paper_manager') or None
+
+    def title(self):
+        return 'Browsing by Paper Manager'
+
+    def content(self):
+        if self.selectedManager is None:
+            return
+        catalog = getToolByName(self.context, 'portal_catalog')
+        return catalog({
+            'meta_type': 'Research',
+            'paper_manager': self.selectedManager,
+            'sort_on': 'sortable_title',
+        })
+
+    def getManagersVocab(self):
+        return self.context.getPaperMembersVocab()
