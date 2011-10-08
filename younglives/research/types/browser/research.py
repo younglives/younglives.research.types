2ae6cd1006f26f4a882f3aa05ec4c6fb8eacff5f
Michael Davis <m.r.davis@me.com>
2011-10-08 16:25:00 +0000

    Add a view for the research type

---
+++ younglives/research/types/browser/research.py
@@ -0,0 +1,19 @@
+from zope.component import getMultiAdapter, getUtility
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import ILocalPortletAssignmentManager
+from plone.portlets.constants import CONTEXT_CATEGORY
+
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+
+class ResearchView(BrowserView):  
+
+    template = ViewPageTemplateFile('templates/research_view.pt')
+
+    def __call__(self):
+        portletManager  = getUtility(IPortletManager, name='plone.leftcolumn')
+        assignable = getMultiAdapter((self.context, portletManager,), ILocalPortletAssignmentManager)
+        assignable.setBlacklistStatus(CONTEXT_CATEGORY, True)
+        return self.template()
+
