817220834a7c60af34a27ba57e791446046e06ee
Michael Davis <m.r.davis@me.com>
2011-10-25 21:59:29 +0000

    Make the internal deadline field clearer and delete the two unused interfaces

--- younglives/research/types/interfaces/transitions.py
+++ younglives/research/types/interfaces/transitions.py
@@ -9,24 +9,9 @@ class IWorkflowForm(Interface):
     """Utility to present a z3c.form during a workflow transition
     """
 
-class IProposedTransition(Interface):
-
-    test_field = schema.Text(
-        title=_(u"Testing"),
-        description=_(u"This is a test."),
-        required=True)
-
-class IAcceptTransition(Interface):
-
-    field = schema.Date(
-        title=_(u"A date field"),
-        description=_(u"This isn't saved anywhere yet"),
-        #default=datetime.date(2007, 4, 1),
-        required=True)
-
 class IDeadlineTransition(Interface):
 
     new_deadline = schema.Date(
-        title=_(u"New Deadline"),
-        description=_(u"Leave this blank to keep the original deadline, the new deadline will be added to the comment"),
+        title=_(u"New Internal Deadline"),
+        description=_(u"Leave this blank to keep the original internal deadline, the new internal deadline will be added to the change comment"),
         required=False)
