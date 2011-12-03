27908df643390d2003542dc8d9d7f4c121b25ea5
Michael Davis <m.r.davis@me.com>
2011-12-03 20:15:34 +0000

    Add a template for the search page

---
+++ younglives/research/types/browser/research_search.py
@@ -0,0 +1,18 @@
+from zope.component import getMultiAdapter, getUtility
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import ILocalPortletAssignmentManager
+from plone.portlets.constants import CONTEXT_CATEGORY
+
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
+
+class ResearchSearch(BrowserView):  
+
+    template = ViewPageTemplateFile('templates/research_search.pt')
+
+    def __call__(self):
+        portletManager  = getUtility(IPortletManager, name='plone.leftcolumn')
+        assignable = getMultiAdapter((self.context, portletManager,), ILocalPortletAssignmentManager)
+        assignable.setBlacklistStatus(CONTEXT_CATEGORY, True)
+        return self.template()
